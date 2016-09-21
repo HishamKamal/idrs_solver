e6c95540fc8f0acd6ebc7ec9cf60a11300d86801
Reinaldo A. Astudillo R <reinaldo@debianome2>
2016-09-21 10:08:18 +0200

    Readme updated, IDRs from scipy version

--- README.md
+++ README.md
@@ -6,67 +6,58 @@ This Python implementation is based on [2]. The interface of the idrs function i
 with the Krylov methods implemented in Scipy.
 
       idrs(A, b, x0=None, tol=1e-5, s=4, maxit=None, xtype=None, M=None, callback=None)
-# Parameter
-    Basic
-    -------
+
+    Parameters
+    ----------
     A : {sparse matrix, dense matrix, LinearOperator}
         The real or complex N-by-N matrix of the linear system.
     b : {array, matrix}
         Right hand side of the linear system. Has shape (N,) or (N,1).
-    Returns
-    -------
-    x : {array, matrix}
-        The converged solution.
-    info : integer
-        Provides convergence information:
-          * 0  : successful exit
-          * >0 : convergence to tolerance not achieved, number of iterations
-          * <0 : illegal input or breakdown (not implemented yet)
-    Other parameters
-    ----------------
-    x0 : {array, matrix}
-        Starting guess for the solution (a vector of zeros by default).
-    tol : float
+    x0  : {array, matrix}
+        Starting guess for the solution.
+    tol : float, optional
         Tolerance to achieve. The algorithm terminates when either the relative
         or the absolute residual is below `tol`.
     s : integer, optional
-        specifies the dimension of the shadow space. Normally, a higher 
+        specifies the dimension of the shadow space. Normally, a higher
         s gives faster convergence, but also makes the method more expensive.
         Default is 4.
     maxiter : integer, optional
-	Maximum number of iterations.  Iteration will stop after maxiter
-	steps even if the specified tolerance has not been achieved.
+        Maximum number of iterations.  Iteration will stop after maxiter
+        steps even if the specified tolerance has not been achieved.
     xtype : {'f','d','F','D'}
-        This parameter is DEPRECATED --- avoid using it.
-        The type of the result.  If None, then it will be determined from
-        A.dtype.char and b.  If A does not have a typecode method then it
-        will compute A.matvec(x0) to get a typecode.   To save the extra
-        computation when A does not have a typecode attribute use xtype=0
-        for the same type as b or use xtype='f','d','F',or 'D'.
-        This parameter has been superseded by LinearOperator.
-    M : {sparse matrix, dense matrix, LinearOperator}
-        Inverse of the preconditioner of A.  M should approximate the
-        inverse of A and be easy to solve.  Effective preconditioning 
-        dramatically improves the rate of convergence, which implies that 
-        fewer iterations are needed to reach a given error tolerance.  
-        By default, no preconditioner is used.
-    callback : function
+        This parameter is deprecated -- avoid using it.
+    M : {sparse matrix, dense matrix, LinearOperator}, optional
+        Preconditioner for A.  The preconditioner should approximate the
+        inverse of A.  Effective preconditioning dramatically improves the
+        rate of convergence, which implies that fewer iterations are needed
+        to reach a given error tolerance.
+    callback : function, optional
         User-supplied function to call after each iteration.  It is called
         as callback(xk), where xk is the current solution vector.
+
+        .. versionadded:: 0.19.0
+
+    Returns
+    -------
+    x : array or matrix
+        The converged solution.
+    info : integer
+        Provides convergence information:
+            - 0  : successful exit
+            - >0 : convergence to tolerance not achieved, number of iterations
+            - <0 : illegal input or breakdown
+
+    References
+    ----------
+
+    .. [1] P. Sonneveld and M. B. van Gijzen
+             SIAM J. Sci. Comput. Vol. 31, No. 2, pp. 1035--1062, (2008).
+    .. [2] M. B. van Gijzen and P. Sonneveld
+             ACM Trans. Math. Software,, Vol. 38, No. 1, pp. 5:1-5:19, (2011).
+    .. [3] This file is a translation of the following MATLAB implementation:
+            http://ta.twi.tudelft.nl/nw/users/gijzen/idrs.m
 
 # License
 
 This software is distributed under the [MIT License](http://opensource.org/licenses/MIT).
-
-# References
-1. IDR(s): a family of simple and fast algorithms for solving large 
-        nonsymmetric linear systems. 
-        P. Sonneveld and M. B. van Gijzen
-        SIAM J. Sci. Comput. Vol. 31, No. 2, pp. 1035--1062, 2008 
-2. Algorithm 913: An Elegant IDR(s) Variant that Efficiently Exploits 
-        Bi-orthogonality Properties. 
-        M. B. van Gijzen and P. Sonneveld
-        ACM Trans. Math. Software,, Vol. 38, No. 1, pp. 5:1-5:19, 2011
-3. This file is a translation of the following MATLAB implementation:
-http://ta.twi.tudelft.nl/nw/users/gijzen/idrs.m
-4. IDR(s)' webpage http://ta.twi.tudelft.nl/nw/users/gijzen/IDR.html
--- idrs_solver/idrs.py
+++ idrs_solver/idrs.py
@@ -31,10 +31,7 @@ __all__ = ['idrs']
 def idrs(A, b, x0=None, tol=1e-5, s=4, maxiter=None, xtype=None,
          M=None, callback=None):
     """
-    Solves the linear system Ax = b using the Induced Dimension Reduction
-    method IDR(s).
-
-    IDR(s) is a short-recurrences Krylov method proposed in [1].
+    Use Induced Dimension Reduction method [IDR(s)] to solve A x = b
 
     Parameters
     ----------
@@ -42,20 +39,9 @@ def idrs(A, b, x0=None, tol=1e-5, s=4, maxiter=None, xtype=None,
         The real or complex N-by-N matrix of the linear system.
     b : {array, matrix}
         Right hand side of the linear system. Has shape (N,) or (N,1).
-    Returns
-    -------
-    x : {array, matrix}
-        The converged solution.
-    info : integer
-        Provides convergence information:
-          * 0  : successful exit
-          * >0 : convergence to tolerance not achieved, number of iterations
-          * <0 : illegal input or breakdown
-    Other parameters
-    ----------------
-    x0 : {array, matrix}
-        Starting guess for the solution (a vector of zeros by default).
-    tol : float
+    x0  : {array, matrix}
+        Starting guess for the solution.
+    tol : float, optional
         Tolerance to achieve. The algorithm terminates when either the relative
         or the absolute residual is below `tol`.
     s : integer, optional
@@ -66,33 +52,38 @@ def idrs(A, b, x0=None, tol=1e-5, s=4, maxiter=None, xtype=None,
         Maximum number of iterations.  Iteration will stop after maxiter
         steps even if the specified tolerance has not been achieved.
     xtype : {'f','d','F','D'}
-        This parameter is DEPRECATED --- avoid using it.
-        The type of the result.  If None, then it will be determined from
-        A.dtype.char and b.  If A does not have a typecode method then it
-        will compute A.matvec(x0) to get a typecode.   To save the extra
-        computation when A does not have a typecode attribute use xtype=0
-        for the same type as b or use xtype='f','d','F',or 'D'.
-        This parameter has been superseded by LinearOperator.
-    M : {sparse matrix, dense matrix, LinearOperator}
-        Inverse of the preconditioner of A.  M should approximate the
-        inverse of A and be easy to solve.  Effective preconditioning
-        dramatically improves the rate of convergence, which implies that
-        fewer iterations are needed to reach a given error tolerance.
-        By default, no preconditioner is used.
-    callback : function
+        This parameter is deprecated -- avoid using it.
+    M : {sparse matrix, dense matrix, LinearOperator}, optional
+        Preconditioner for A.  The preconditioner should approximate the
+        inverse of A.  Effective preconditioning dramatically improves the
+        rate of convergence, which implies that fewer iterations are needed
+        to reach a given error tolerance.
+    callback : function, optional
         User-supplied function to call after each iteration.  It is called
         as callback(xk), where xk is the current solution vector.
+
+        .. versionadded:: 0.19.0
+
+    Returns
+    -------
+    x : array or matrix
+        The converged solution.
+    info : integer
+        Provides convergence information:
+            - 0  : successful exit
+            - >0 : convergence to tolerance not achieved, number of iterations
+            - <0 : illegal input or breakdown
+
     References
     ----------
-    [1] IDR(s): a family of simple and fast algorithms for solving large
-        nonsymmetric linear systems. P. Sonneveld and M. B. van Gijzen
-        SIAM J. Sci. Comput. Vol. 31, No. 2, pp. 1035--1062, 2008
-    [2] Algorithm 913: An Elegant IDR(s) Variant that Efficiently Exploits
-        Bi-orthogonality Properties. M. B. van Gijzen and P. Sonneveld
-        ACM Trans. Math. Software,, Vol. 38, No. 1, pp. 5:1-5:19, 2011
-    [3] This file is a translation of the following MATLAB implementation:
-        http://ta.twi.tudelft.nl/nw/users/gijzen/idrs.m
-    [4] IDR(s)' webpage http://ta.twi.tudelft.nl/nw/users/gijzen/IDR.html
+
+    .. [1] P. Sonneveld and M. B. van Gijzen
+             SIAM J. Sci. Comput. Vol. 31, No. 2, pp. 1035--1062, (2008).
+    .. [2] M. B. van Gijzen and P. Sonneveld
+             ACM Trans. Math. Software,, Vol. 38, No. 1, pp. 5:1-5:19, (2011).
+    .. [3] This file is a translation of the following MATLAB implementation:
+            http://ta.twi.tudelft.nl/nw/users/gijzen/idrs.m
+
     """
     A, M, x, b, postprocess = make_system(A, M, x0, b, xtype)
 
@@ -104,29 +95,27 @@ def idrs(A, b, x0=None, tol=1e-5, s=4, maxiter=None, xtype=None,
     psolve = M.matvec
     xtype = x.dtype
 
-    axpy, dot, scal, norm = get_blas_funcs(
-        ['axpy', 'dot', 'scal', 'nrm2'], dtype=xtype)
+    axpy, dot, scal = get_blas_funcs(['axpy', 'dot', 'scal'], dtype=xtype)
 
     np.random.seed(0)
     P = np.random.rand(s, n)
-    bnrm = norm(b)
+    bnrm = np.linalg.norm(b)
     info = 0
 
-#   Check for zero rhs:
+    # Check for zero rhs:
     if bnrm == 0.0:
-        #   Solution is null-vector
+        # Solution is null-vector
         return postprocess(np.zeros(n, dtype=xtype)), 0
-#   Compute initial residual:
+    # Compute initial residual:
     r = b - matvec(x)
-    rnrm = norm(r)
-#   Relative tolerance
-    tolb = tol * bnrm
-
+    rnrm = np.linalg.norm(r)
+    # Relative tolerance
+    tolb = tol*np.linalg.norm(b)
     if rnrm < tolb:
-        #   Initial guess is a good enough solution
+        # Initial guess is a good enough solution
         return postprocess(x), 0
 
-#   Initialization
+    # Initialization
     angle = 0.7
     G = np.zeros((n, s), dtype=xtype)
     U = np.zeros((n, s), dtype=xtype)
@@ -134,69 +123,72 @@ def idrs(A, b, x0=None, tol=1e-5, s=4, maxiter=None, xtype=None,
     om = 1.0
     iter_ = 0
 
-#   Main iteration loop, build G-spaces:
-    while rnrm > tolb and iter_ < maxiter:
-        #   New right-hand size for small system:
+    # Main iteration loop, build G-spaces:
+    while rnrm >= tolb and iter_ < maxiter:
+        # New right-hand size for small system:
         f = P.dot(r)
         for k in xrange(s):
-#           Solve small system and make v orthogonal to P:
+            # Solve small system and make v orthogonal to P:
             c = np.linalg.solve(Ms[k:s, k:s], f[k:s])
             v = r - G[:, k:s].dot(c)
-#           Preconditioning:
+            # Preconditioning:
             v = psolve(v)
-#           Compute new U(:,k) and G(:,k), G(:,k) is in space G_j
+            # Compute new U(:,k) and G(:,k), G(:,k) is in space G_j
             U[:, k] = axpy(v, U[:, k:s].dot(c), None, om)
-#           Matrix-vector product
+            # Matrix-vector product
             G[:, k] = matvec(U[:, k])
-
-#           Bi-Orthogonalize the new basis vectors:
-            for i in range(0, k):
+            # Bi-Orthogonalize the new basis vectors:
+            for i in xrange(0, k):
                 alpha = dot(P[i, :], G[:, k]) / Ms[i, i]
                 G[:, k] = axpy(G[:, i], G[:, k], None, -alpha)
                 U[:, k] = axpy(U[:, i], U[:, k], None, -alpha)
 
-#           New column of M = P'*G  (first k-1 entries are zero)
-            for i in range(k, s):
+            # New column of M = P'*G  (first k-1 entries are zero)
+            for i in xrange(k, s):
                 Ms[i, k] = dot(P[i, :], G[:, k])
 
-#           Make r orthogonal to q_i, i = 1..k
+            if Ms[k, k] == 0.0:
+                # Breakdown
+                return postprocess(x), -1
+
+            # Make r orthogonal to q_i, i = 1..k
             beta = f[k] / Ms[k, k]
             x = axpy(U[:, k], x, None, beta)
             r = axpy(G[:, k], r, None, -beta)
-            rnrm = norm(r)
+            rnrm = np.linalg.norm(r)
             if callback is not None:
                 callback(x)
             iter_ += 1
             if rnrm < tolb or iter_ >= maxiter:
                 break
-#           New f = P'*r (first k  components are zero)
+            # New f = P'*r (first k  components are zero)
             if k < s - 1:
                 f[k + 1:s] = axpy(Ms[k + 1:s, k], f[k + 1:s], None, -beta)
-# Now we have sufficient vectors in G_j to compute residual in G_j+1
-# Note: r is already perpendicular to P so v = r
+
+        # Now we have sufficient vectors in G_j to compute residual in G_j+1
+        # Note: r is already perpendicular to P so v = r
         if rnrm < tolb or iter_ >= maxiter:
             break
-#       Preconditioning:
+        # Preconditioning:
         v = psolve(r)
-#       Matrix-vector product
+        # Matrix-vector product
         t = matvec(v)
-#       Computation of a new omega
-        ns = norm(r)
-        nt = norm(t)
+        # Computation of a new omega
+        ns = np.linalg.norm(r)
+        nt = np.linalg.norm(t)
         ts = dot(t, r)
         rho = abs(ts / (nt * ns))
         om = ts / (nt * nt)
         if rho < angle:
             om = om * angle / rho
-#       New vector in G_j+1
+        # New vector in G_j+1
         x = axpy(v, x, None, om)
         r = axpy(t, r, None, -om)
-        rnrm = norm(r)
+        rnrm = np.linalg.norm(r)
         if callback is not None:
             callback(x)
         iter_ += 1
 
-    if rnrm > tolb:
-    # info isn't set appropriately otherwise        
+    if rnrm >= tolb:
         info = iter_
     return postprocess(x), info
